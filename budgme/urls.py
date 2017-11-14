9ca54082265600e83c0823a7ec4e310839f61879
psv <psv.programmer@gmail.com>
2017-11-14 17:06:36 +0200

    bootstrap added first test

--- budgme/urls.py
+++ budgme/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
 from django.contrib import admin
 
 from budgme import views
@@ -22,3 +24,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home')
 ]
+
+
+# urlpatterns += staticfiles_urlpatterns()
+# # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
