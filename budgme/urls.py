a029345e5b1417edd780d3a4aba7f1eee66292ed
psv <psv.programmer@gmail.com>
2017-12-01 00:30:59 +0200

    test

--- budgme/urls.py
+++ budgme/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # if not settings.DEBUG:
 #     urlpatterns += static('',
