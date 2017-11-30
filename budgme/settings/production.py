1fe69f2d72e4e5f91aec38cd2f79310a4b9a4fb6
psv <psv.programmer@gmail.com>
2017-11-30 22:57:07 +0200

    test

--- budgme/settings/production.py
+++ budgme/settings/production.py
@@ -126,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'budgme', 'static').replace('\\', '/')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
