04cbafe338f2caef9d9eb26513985a151020a890
psv <psv.programmer@gmail.com>
2017-12-14 19:02:52 +0200

    testing profile page

--- budgme/static/budgme/js/global_script.js
+++ budgme/static/budgme/js/global_script.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
   // $("div.fadeout-slowly-3").fadeOut(3000);
-  $("div.fadeout-slowly-3").css({opacity: 1.0, visibility: "visible"}).animate({opacity: 0.0},3000);
+  $("span.fadeout-slowly-3").css({opacity: 1.0, visibility: "visible"}).animate({opacity: 0.0},3000);
 });
